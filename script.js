d6d5e25becd8b192803c3e720506495fdd957885
Tamir Razon <13859192+Tamir107@users.noreply.github.com>
2024-01-05 17:17:12 +0200

    Add getComputerChoice function

---
+++ script.js
@@ -0,0 +1,9 @@
+function getComputerChoice(){
+    let choice = Math.floor(Math.random() * 3) + 1;
+    switch(choice){
+        case 1: return 'Rock';
+        case 2: return 'Paper';
+        case 3: return 'Scissors';
+    }
+}
+
